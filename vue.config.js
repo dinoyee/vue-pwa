f087e38383689838a4adaf602f44a3c9609fc404
dinoyee <yeederan1991@gmail.com>
2022-04-01 02:14:35 +0800

    feat: add pwa config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,15 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/vue-pwa/" : "/",
+  pwa: {
+    name: "vue-pwa",
+    themeColor: "#4DBA87",
+    msTileColor: "#FFFFFF",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      skipWaiting: true,
+    },
+  },
 });
